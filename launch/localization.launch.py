5516fde4223b4e5abd387f1b3e807702381fa8b0
Max Polzin <polzin.max@gmail.com>
2023-11-19 11:38:18 +0100

    Launch complementary filter.

--- launch/localization.launch.py
+++ launch/localization.launch.py
@@ -27,17 +27,19 @@ def generate_launch_description():
         name='navsat_transform_node',
         output='screen',
         parameters=[{
-            "magnetic_declination_radians": 0.0,
+            "magnetic_declination_radians": 0.05,
             "yaw_offset": 0.0,
-            "zero_altitude": True,
+            "zero_altitude": False,
             "use_odometry_yaw": False,
             "wait_for_datum": False,
             "publish_filtered_gps": False,
             "broadcast_utm_transform": False,
             "use_simtime": True,
+            "delay": 3.0,
         }],
         remappings=[
             ('/odometry/filtered', '/odom'),
+            ('/imu','/imu/data'),
         ]
         )
 
@@ -53,8 +55,25 @@ def generate_launch_description():
         ]
         )
 
+
+    complementary_filter_node = Node(
+                package='imu_complementary_filter',
+                executable='complementary_filter_node',
+                name='complementary_filter_gain_node',
+                output='screen',
+                parameters=[
+                    {'do_bias_estimation': True},
+                    {'do_adaptive_gain': True},
+                    {'use_mag': True},
+                    {'gain_acc': 0.1},
+                    {'gain_mag': 0.1},
+                ],
+            )
+
+
     return LaunchDescription(
         [
+            complementary_filter_node,
             ukf_localization_node,
             navsat_transform_node,
             map_transform_node,
