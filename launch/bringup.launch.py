c24c862b727203ac2eaf22ab3cacdcf434e6b15b
Max Polzin <polzin.max@gmail.com>
2024-04-18 19:02:58 +0200

    Committing hanging changes.

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -7,7 +7,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
@@ -23,20 +23,22 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/visualization.launch.py'))
     )
 
-    localization = IncludeLaunchDescription(
+    localization = TimerAction(period=5.0, actions=[IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/localization.launch.py'))
+        )]
     )
 
-    navigation = IncludeLaunchDescription(
+    navigation = TimerAction(period=10.0, actions=[IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/navigation.launch.py'))
+        )]
     )
 
     return LaunchDescription(
         [
             simulation,
             visualization,
-            # localization,
-            # navigation,
+            localization,
+            navigation
         ]
     )
 
